77fc71313e05f206d09ab7cec6a2293f8bd223fd
liam-goodman <liam.goodman@vodafone.com>
2026-03-17 22:05:33 +0100

    feat: Remove all unused code

--- backend/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthGuard } from './auth.guard';
-import { RolesGuard } from './roles.guard';
-
-@Module({
-  providers: [AuthGuard, RolesGuard],
-  exports: [AuthGuard, RolesGuard],
-})
-export class AuthModule {}
--- backend/src/auth/roles.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-// Decorator: Markiert einen Endpoint mit den erlaubten Rollen.
-// Verwendung: @Roles('admin') über einem Controller-Endpoint.
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
